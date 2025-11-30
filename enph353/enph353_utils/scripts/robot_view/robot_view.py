727ad091056937aa578f1f2769f3cbd4e3588d2f
248nonny <ronny.cravioto@gmail.com>
2025-11-30 15:01:14 -0800

    Sucessfully reading clues and turning them into stringsgit status!

--- enph353/enph353_utils/scripts/robot_view/robot_view.py
+++ enph353/enph353_utils/scripts/robot_view/robot_view.py
@@ -14,6 +14,8 @@ from python_qt_binding import loadUi
 from ultralytics import YOLO
 from ultralytics.nn.tasks import DetectionModel
 import torch
+import numpy as np
+from sklearn.cluster import DBSCAN
 
 
 class RobotViewApp(QtWidgets.QMainWindow):
@@ -29,6 +31,8 @@ class RobotViewApp(QtWidgets.QMainWindow):
         # self.yolo_model.load_state_dict(ckpt["model"])
 
         self.bridge = CvBridge()
+
+        self.latest_clue = ("", "")
 
         # TODO: figure out what exactly to subscribe to here.
         self.image_sub = rospy.Subscriber(
@@ -83,7 +87,87 @@ class RobotViewApp(QtWidgets.QMainWindow):
     def SLOT_save_images_button_handler(self):
         self.save_current_image()
 
-    def draw_yolo12_detections_bgr(self, img_bgr, conf_thres=0.25, iou_thres=0.45):
+    def process_clue(self, boxes):
+        names = self.yolo_model.names
+        data = list(zip((names[id] for id in boxes.cls.tolist()), boxes.xywhn.tolist()))
+
+        data = [t for t in data if t[0] != "sign"]
+
+        if len(data) == 0:
+            return
+
+        # Cluster by distance
+        coords = np.array([np.array([t[1][0], t[1][1]]) for t in data])
+
+        db = DBSCAN(eps=0.14, min_samples=2).fit(coords)
+
+        clusters = {}
+
+        for label in db.labels_:
+            clusters[label] = []
+
+        for pair, label in zip(data, db.labels_):
+            if label == -1:
+                continue
+            clusters[label].append(pair)
+
+        clusters_with_centroid = []
+
+        for label, items in clusters.items():
+            if len(items) == 0:
+                continue
+
+            xy = np.array([[item[1][0], item[1][1]] for item in items])
+            centroid = xy.mean(axis=0)
+
+            clusters_with_centroid.append(
+                (centroid, sorted(items, key=lambda t: t[1][0]))
+            )
+
+        # print(clusters_with_centroid)
+
+        sorted_clusters = sorted(clusters_with_centroid, key=lambda t: t[0][1])
+
+        clue_type = "".join([t[0] for t in sorted_clusters[0][1]])
+
+        sorted_clusters = sorted_clusters[1:]
+
+        sorted_clusters = sorted(sorted_clusters, key=lambda t: t[0][0])
+
+        # clue = " ".join(
+        # ["".join([[t[0] for t in cluster[1]] for cluster in sorted_clusters])]
+        # )
+        clue = [[t[0] for t in cluster[1]] for cluster in sorted_clusters]
+        clue = ["".join(x) for x in clue]
+        clue = " ".join(clue)
+
+        # [[t[0] for t in cluster[1]] for cluster in sorted_clusters]
+
+        print("clue type: " + clue_type)
+        print("clue: " + clue)
+
+        self.latest_clue = (clue_type, clue)
+
+        self.clue_type_label.setText(clue_type)
+        self.clue_label.setText(clue)
+
+        # print("Clue type: " + clue_type)
+        # print("Clue: " + clue)
+
+        # print([[t[0] for t in cluster[1]] for cluster in sorted_clusters])
+
+        # output_string = "".join(t[0] for t in sorted_data)
+        # print(output_string)
+
+    def draw_yolo12_detections_bgr(
+        self,
+        img_bgr,
+        conf_thres=0.25,
+        iou_thres=0.45,
+        recursed=False,
+        text_scale=1.4,
+        text_line_thickness=2,
+    ):
         """
         img_bgr: numpy array in BGR (as from cv2.imread / camera)
         returns: new BGR image with boxes and labels drawn
@@ -107,6 +191,9 @@ class RobotViewApp(QtWidgets.QMainWindow):
         boxes = r.boxes  # Boxes object
         names = self.yolo_model.names  # dict: class_id -> class_name
 
+        if recursed:
+            self.process_clue(boxes)
+
         for box in boxes:
             cls_id = int(box.cls[0].item())
             conf = float(box.conf[0].item())
@@ -115,13 +202,32 @@ class RobotViewApp(QtWidgets.QMainWindow):
             # Convert to int for OpenCV
             x0, y0, x1, y1 = map(int, (x0, y0, x1, y1))
 
+            # If sign is detected and this is top level call, recurse into each sign.
+            if cls_id == 36 and not recursed:
+                print("Recursing to read sign!")
+
+                self.annotated_sign_img = self.draw_yolo12_detections_bgr(
+                    img_bgr[y0:y1, x0:x1],
+                    recursed=True,
+                    text_scale=0.4,
+                    text_line_thickness=1,
+                )
+
+                # print(self.raw_cv_image.shape)
+                resized = cv2.resize(self.annotated_sign_img, (400, 261))
+                pixmap = self.convert_cv_to_pixmap(resized)
+                # print(resized.shape)
+                self.sign_image_label.setPixmap(pixmap)
+
             label = f"{names.get(cls_id, cls_id)} {conf:.2f}"
 
             # Draw rectangle
             cv2.rectangle(im_draw, (x0, y0), (x1, y1), (0, 255, 0), 2)
 
             # Draw label background
-            (tw, th), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+            (tw, th), _ = cv2.getTextSize(
+                label, cv2.FONT_HERSHEY_SIMPLEX, text_scale, text_line_thickness
+            )
             th = int(th * 1.4)
             cv2.rectangle(im_draw, (x0, y0 - th), (x0 + tw, y0), (0, 255, 0), -1)
 
@@ -131,9 +237,9 @@ class RobotViewApp(QtWidgets.QMainWindow):
                 label,
                 (x0, y0 - 5),
                 cv2.FONT_HERSHEY_SIMPLEX,
-                0.6,
+                text_scale,
                 (0, 0, 0),
-                1,
+                text_line_thickness,
                 cv2.LINE_AA,
             )
 
